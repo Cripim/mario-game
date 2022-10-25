7ccc79cd26f5fc909af63d9ff6e80c6ba1ddf89c
Diego Tavares <mello.gredy@gmail.com>
2022-10-25 01:32:42 -0300

    💫 :dizzy: Nova Animação
    Adcionando mais um cano e definindo uma animação no jogo.

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 const mario = document.querySelectorAll(".mario")[0];
 const pipe = document.querySelectorAll(".pipe")[0];
+const pipe2 = document.querySelectorAll(".pipe2")[0];
 const jump = () => {
   mario.classList.add("jump");
   setTimeout(() => {
@@ -9,12 +10,26 @@ const jump = () => {
 const loop = setInterval(() => {
   console.log("loop");
   const pipePosition = pipe.offsetLeft;
+  const pipePosition2 = pipe2.offsetLeft;
   const marioPosition = +window
     .getComputedStyle(mario)
     .bottom.replace("px", "");
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.left = `${marioPosition}px`;
+
+    mario.src = "./assets/img/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "35px";
+    clear.Interval("loop");
+  }
+
+  if (pipePosition2 <= 120 && pipePosition2 > 0 && marioPosition < 80) {
+    pipe2.style.animation = "none";
+    pipe2.style.left = `${pipePosition2}px`;
 
     mario.style.animation = "none";
     mario.style.left = `${marioPosition}px`;
